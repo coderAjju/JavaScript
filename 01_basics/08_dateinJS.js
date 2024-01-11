db345ff2e20bda0ed653af375c6272c930ba8738
Ajay Upadhyay <156045848+coderAjju@users.noreply.github.com>
2024-01-11 12:45:48 +0000

    date in js

---
+++ 01_basics/08_dateinJS.js
@@ -0,0 +1,11 @@
+let date = new Date();
+// console.log(date.toDateString())   // Thu Jan 11 2024
+// console.log(date.toLocaleDateString())   // 1/11/2024
+// console.log(date.toLocaleString());     // 1/11/2024, 12:12:18 PM
+// console.log(date.toString());
+// console.log(date.toLocaleTimeString()); // 12:16:58 PM
+
+let created_Date = new Date(2002,2,19);
+console.log(created_Date.toDateString());
+console.log(created_Date.getDay()); //it will print day such as 1 for monday 2 for tuesday ...... 7 for sunday
+
