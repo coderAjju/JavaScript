df28395b5faff3f5a63410174316d2f9a16f6536
Ajay Upadhyay <156045848+coderAjju@users.noreply.github.com>
2024-01-17 05:08:14 +0000

    IIFE function

---
+++ 03_basics/05_IIFE.js
@@ -0,0 +1,26 @@
+// Immediately Invoked Function Expression
+
+function chai(){
+    console.log("hello");
+}
+chai();  // ye to mera regular function hai
+
+(
+    function(){ // ese named IIFE function kahte hain kabhi kabhi interviewer puch leta hain
+        console.log("IIFE syntax")
+    }
+)(); // IIFE function ko nhi pata hota hai ki usse function kaha khatam karna hai essi liye IIFE function ke end main semicolon(;) lagana jaroori hai. agar aap uske just baad phir se ek IIFE function likh rhe hai.
+
+(
+    ()=>{ // aur ese unnamed IIFE function kahte hain.
+        console.log("IIFE second syntax")
+    }
+)();
+
+
+//IIFE function main parameter kaise pass karte hain ye bhi jaan lete hain
+(
+    function (name){   //kuch es tarah se parameter pass kiye jate hain
+        console.log(`hello my name is ${name}`)
+    }
+)('ajay');
