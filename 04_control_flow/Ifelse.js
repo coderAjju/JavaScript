1e4c501fb881c164775cd2968ad98b8b78761704
Ajay Upadhyay <156045848+coderAjju@users.noreply.github.com>
2024-01-19 06:07:36 +0000

    updated

---
+++ 04_control_flow/Ifelse.js
@@ -0,0 +1,23 @@
+//--------- if-else statement-----------
+function if_else() {
+  let age = 20;
+  if (age >= 18) {
+    console.log("you can drive car");
+  } else {
+    console.log("you can not drive car");
+  }
+}
+if_else();
+// ------------ if-elseif-else statement-------------
+function if_elseif_else() {
+  let tempareture = 30;
+  if (tempareture > 41) {
+    console.log("the tempareture is above 41 degree");
+  } else if (tempareture == 41) {
+    console.log("the tempareture is 41 degree");
+  } else {
+    console.log("the temparetur is less than 41 degree");
+  }
+}
+if_elseif_else();
+
