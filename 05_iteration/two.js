3ba9bec3efef37129629f4d82644ca1938ad1b6d
Ajay Upadhyay <156045848+coderAjju@users.noreply.github.com>
2024-01-20 07:42:19 +0000

    while and do-while

---
+++ 05_iteration/two.js
@@ -0,0 +1,15 @@
+let a = 1;
+while(a<=10){
+    if(a==5){
+        break;
+    }
+    // console.log(a);
+    a++;
+}
+
+let score = 1;
+
+do{
+    console.log(score);
+    score++;
+}while(score<=10);
